0c983fc9f74661b96909bece5996d626acfdaa45
Alprince Nacion <nacionalprince@gmail.com>
2022-12-12 18:32:31 +0800

    Final ShoppingCart Done!

--- shopping-cart/function.php
+++ shopping-cart/function.php
@@ -2,7 +2,7 @@
 	DEFINE("DB_SERVER", "localhost");
 	DEFINE("DB_USERNAME", "root");
 	DEFINE("DB_PASSWORD", "");
-	DEFINE("DB_NAME", "shopping_cart");
+	DEFINE("DB_NAME", "shoppingcart3b");
 
 	function openConnection(){
 		$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
